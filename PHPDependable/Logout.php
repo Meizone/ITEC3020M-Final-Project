c8976e43b9f467ac1371e7ce40935bc28b351b44
Kroynix <zhectx@gmail.com>
2024-05-28 07:39:09 -0400

    Missing 'user_id' in logout function for unset

--- PHPDependable/Logout.php
+++ PHPDependable/Logout.php
@@ -3,8 +3,8 @@
 
 session_start();
 
-if(isset($_SESSION[""])){   
-    unset($_SESSION[""]);
+if(isset($_SESSION['user_id'])){   
+    unset($_SESSION['user_id']);
 }
 
 header("Location: /RegistrationForms/Login.php");
